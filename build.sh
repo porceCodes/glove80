6a97082ede44aa2ad3caddec1fd6074869a60f64
Jiwoong Lee <jiwoong_lee@apple.com>
2025-09-13 14:42:05 -0700

    Simplify build logs

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 IMAGE=glove80-zmk-config-docker
 BRANCH="${1:-main}"
 
-docker build -t "$IMAGE" .
-docker run --rm -v "$PWD:/config" -e UID="$(id -u)" -e GID="$(id -g)" -e BRANCH="$BRANCH" "$IMAGE"
+docker build -q -t "$IMAGE" . 2>&1 # | grep "glove80.keymap:"
+docker run --rm -v "$PWD:/config" -e UID="$(id -u)" -e GID="$(id -g)" -e BRANCH="$BRANCH" "$IMAGE" 2>&1 | grep "glove80.keymap:"
