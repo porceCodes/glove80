9aac244b1b2b59227b630998465f989403137ff6
Jiwoong Lee <jiwoong_lee@apple.com>
2025-10-05 09:55:01 -0700

    Add language toggle

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 IMAGE=glove80-zmk-config-docker
 BRANCH="${1:-main}"
 
+open -g -a Docker
 docker build -q -t "$IMAGE" . 2>&1 # | grep "glove80.keymap:"
 docker run --rm -v "$PWD:/config" -e UID="$(id -u)" -e GID="$(id -g)" -e BRANCH="$BRANCH" "$IMAGE" 2>&1 | grep "glove80.keymap:"
